03930272e92d0b9d1c9140b6f74862a8202a9b0c
tsakina <117813897+tsakina@users.noreply.github.com>
2023-06-12 10:20:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,120 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+MOVIE_API_KEY = "0fdffdc00d9cf9bd43a7a98249bbd65b"
+API_READ_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZmRmZmRjMDBkOWNmOWJkNDNhN2E5ODI0OWJiZDY1YiIsInN1YiI6IjY0ODAzZTc0ZDJiMjA5MDBjYTFjYzNmNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Oe9pjDYCcQV7m0jTuBYrQ4JtHUt05B36lgkL-0IMvEg"
+TMDB_URL = "https://api.themoviedb.org/3/search/movie"
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+bootstrap = Bootstrap5(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies-collection.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.app_context().push()
+db = SQLAlchemy(app)
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float)
+    ranking = db.Column(db.Integer)
+    review = db.Column(db.String(250))
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+class EditMovieForm(FlaskForm):
+    rating = StringField(label="Your Rating Out of 10 e.g. 7.5", validators=[DataRequired()])
+    review = StringField(label="Your Review", validators=[DataRequired()])
+    submit = SubmitField(label="Done")
+
+
+class AddMovieForm(FlaskForm):
+    title = StringField(label="Movie Title", validators=[DataRequired()])
+    submit = SubmitField(label="Add Movie")
+
+
+@app.route("/")
+def home():
+    all_movies = db.session.execute(db.select(Movie).order_by(Movie.rating)).scalars().all()
+    for i in range(len(all_movies)):
+        # This line gives each movie a new ranking reversed from their order in all_movies
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
+    return render_template("index.html", movies=all_movies)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_movie():
+    form = AddMovieForm()
+    if form.validate_on_submit():
+        movie_title = form.title.data
+        response = requests.get(url=TMDB_URL, params={"query": movie_title, "api_key": MOVIE_API_KEY})
+        movie_list = response.json()["results"]
+        return render_template("select.html", movie_list=movie_list)
+    return render_template("add.html", form=form)
+
+
+@app.route("/edit/<movie_id>", methods=["GET", "POST"])
+def edit(movie_id):
+    movie = db.get_or_404(Movie, movie_id)
+    form = EditMovieForm()
+    if form.validate_on_submit():
+        movie.rating = float(form.rating.data)
+        movie.review = form.review.data
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("edit.html", movie=movie, form=form)
+
+
+@app.route("/select/<movie_id>")
+def select(movie_id):
+    response = requests.get(url=f"https://api.themoviedb.org/3/movie/{movie_id}", params={"api_key": MOVIE_API_KEY})
+    result = response.json()
+    new_movie = Movie(
+        title=result["title"],
+        year=result["release_date"].split("-")[0],
+        description=result["overview"],
+        img_url=f"https://image.tmdb.org/t/p/w500{result['poster_path']}"
+    )
+    db.session.add(new_movie)
+    db.session.commit()
+    movie_id = new_movie.id
+    return redirect(url_for("edit", movie_id=movie_id))
+
+
+@app.route("/delete/<movie_id>")
+def delete(movie_id):
+    movie = db.get_or_404(Movie, movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# new_movie = Movie(
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+# )
+# db.session.add(new_movie)
+# db.session.commit()
